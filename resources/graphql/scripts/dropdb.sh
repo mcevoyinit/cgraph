11e15e344d5e74791f57890acecf75ee66b9f662
Eric McEvoy <eric.mcevoy@sixdx.com>
2021-07-11 17:07:51 +0000

    Deployed 30f29a0 with MkDocs version: 1.1.2

---
+++ resources/graphql/scripts/dropdb.sh
@@ -0,0 +1,3 @@
+#bin/bash
+
+curl -X POST -H 'Content-Type: application/json' -H 'X-Auth-Token:YjNkN2EwMThjMzhjMDMzM2I1MDFmNWM3ZDE2YTI5YWM=' https://dawn-sky.eu-west-1.aws.cloud.dgraph.io/alter -d '{"drop_op": "DATA"}'
