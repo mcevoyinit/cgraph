96f12806bbc10b8bae1995dfebc62626871a089f
Eric McEvoy <eric.mcevoy@sixdx.com>
2021-07-20 21:32:08 +0100

    comb dependencies

--- cgraph-core/src/main/kotlin/com/cgraph/core/support/Testing.kt
+++ cgraph-core/src/main/kotlin/com/cgraph/core/support/Testing.kt
@@ -1,8 +1,6 @@
 package com.cgraph.core.support
 
 import com.cgraph.core.services.CGraphService
-import com.cgraph.core.services.GraphQLRequestType
 import net.corda.testing.driver.InProcess
-import kotlin.test.assertNotNull
 
 fun InProcess.cgraph() = services.cordaService(CGraphService::class.java)
